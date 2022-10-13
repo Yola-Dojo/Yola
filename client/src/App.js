ac0cfb071fe6011025d3c6b97022b83bab4d30f7
Bryan Myers <bryanmyers831@gmail.com>
2022-10-13 12:17:02 -0700

    Routes with basic layout elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Header />}></Route>
+        <Route path={'/products'} element={<Header />}></Route>
+        <Route path={'/checkout'} element={<Header />}></Route>
+        <Route path={'/admin'} element={<Header />}></Route>
+      </Routes>
+      <main>
+        <Routes>
+          <Route path={'/'} element={null}></Route>
+          <Route path={'/login'} element={null}></Route>
+          <Route path={'/register/consumer'} element={null}></Route>
+          <Route path={'/checkout'} element={null}></Route>
+          <Route path={'/admin'} element={null}></Route>
+          <Route path={'/admin/create'} element={null}></Route>
+          <Route path={'/admin/edit/:id'} element={null}></Route>
+          <Route path={'/create'} element={null}></Route>
+          <Route path={'/products'} element={null}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>Nav element</nav>
+  )
+}
+
+export default Nav
