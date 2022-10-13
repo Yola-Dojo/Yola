f5714af1f07d08ef9c5a381ad0eb78def488d9ee
Alex Valdez <xvaldez0411@gmail.com>
2022-10-12 21:34:59 -0500

    added server.js file

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser()) 
+
+require('./config/mongoose.config')
+
+require('./routes/product.route')(app)
+require('./routes/user.route')(app) //added for LR
+
+
+
+app.listen(process.env.MY_PORT, () => console.log(`Locked and loaded on port: ${process.env.MY_PORT}`))
