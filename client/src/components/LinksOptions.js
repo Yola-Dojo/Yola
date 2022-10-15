4471da2592f75fc615bc3a230ab2b997f49c002a
Bryan Myers <bryanmyers831@gmail.com>
2022-10-14 22:26:50 -0700

    reg modal implemented

--- client/src/components/LinksOptions.js
+++ client/src/components/LinksOptions.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import CartIcon from '../components/CartIcon.svg'
+import Register from './Register'
 
 const LinksOptions = () => {
+
+  const openDialog = () => {document.getElementById('regDialog').showModal()}
+  const closeDialog = () => {document.getElementById('regDialog').close()}
+
   return (
-    <ul className='inline stageRight'>
-      <li><Link to='/login' className='noWrap'>Sign in</Link></li>
-      <li><Link to='/register'>Register</Link></li>
-      <li><Link to='/'><img src={CartIcon} className='cartIcon' /></Link></li>
-    </ul>
+    <>
+      <ul className='inline stageRight'>
+        <li><Link to='/login' className='noWrap'>Sign in</Link></li>
+        <li><button className='link' onClick={openDialog}>Register</button></li>
+        <li><Link to='/'><img src={CartIcon} className='cartIcon' /></Link></li>
+      </ul>
+      <dialog id='regDialog'>
+        <button className='closer' onClick={closeDialog}>X</button>
+        <Register/>
+      </dialog>
+    </>
   )
 }
 
