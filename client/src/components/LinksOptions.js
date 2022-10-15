fd3cef8a857d040c16023e257be745f4ae0b107c
Bryan Myers <bryanmyers831@gmail.com>
2022-10-15 14:47:04 -0700

    reg working from login page, other fixes

--- client/src/components/LinksOptions.js
+++ client/src/components/LinksOptions.js
@@ -6,19 +6,15 @@ import Register from './Register'
 const LinksOptions = () => {
 
   const openDialog = () => {document.getElementById('regDialog').showModal()}
-  const closeDialog = () => {document.getElementById('regDialog').close()}
 
   return (
     <>
       <ul className='inline stageRight'>
         <li><Link to='/login' className='noWrap'>Sign in</Link></li>
         <li><button className='link' onClick={openDialog}>Register</button></li>
-        <li><Link to='/'><img src={CartIcon} className='cartIcon' /></Link></li>
+        <li><Link to='/'><img src={CartIcon} className='cartIcon' alt='Your Shopping Cart' /></Link></li>
       </ul>
-      <dialog id='regDialog'>
-        <button className='closer' onClick={closeDialog}>X</button>
-        <Register/>
-      </dialog>
+      <Register/>
     </>
   )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -7,6 +7,8 @@ const Register = () => {
     const [confirmReg, setConfirmReg] = useState("")
     const [errors, setErrors] = useState({})
 
+    const closeDialog = () => {document.getElementById('regDialog').close()}
+
     const [user, setUser] = useState({
         firstName:"",
         lastName:"",
@@ -50,72 +52,75 @@ const Register = () => {
         })
     }
   return (
-    <div className='reg-form'>
-        <h2>Sign Up</h2>
-        <p style={{fontStyle:'italic', fontSize:'12px'}}>Please fill in this form to create an account</p>
-      {confirmReg ? <h4 style={{color: 'green'}}>{confirmReg}</h4> : null}
-      <form onSubmit={register}>
-        {errors.firstName ? (<span className='error-text'>{errors.firstName.message}</span>) : null}
-        <div className='reg-top'>
-            <input
-            placeholder='First name'
-            type='text'
-            name='firstName'
-            value = {user.firstName}
-            onChange={(e)=> handleChange(e)}
-            />
+    <dialog id='regDialog'>
+      <button className='closer' onClick={closeDialog}>X</button>
+        <div className='reg-form'>
+            <h2>Sign Up</h2>
+            <p style={{fontStyle:'italic', fontSize:'12px'}}>Please fill in this form to create an account</p>
+        {confirmReg ? <h4 style={{color: 'green'}}>{confirmReg}</h4> : null}
+        <form onSubmit={register}>
+            {errors.firstName ? (<span className='error-text'>{errors.firstName.message}</span>) : null}
+            <div className='reg-top'>
+                <input
+                placeholder='First name'
+                type='text'
+                name='firstName'
+                value = {user.firstName}
+                onChange={(e)=> handleChange(e)}
+                />
 
-            {errors.lastName ? (<span className='error-text'>{errors.lastName.message}</span>) : null}
-            <input
-            placeholder='Last Name'
-            type='text'
-            name='lastName'
-            value={user.lastName}
-            onChange={(e)=> handleChange(e)}
-            />
+                {errors.lastName ? (<span className='error-text'>{errors.lastName.message}</span>) : null}
+                <input
+                placeholder='Last Name'
+                type='text'
+                name='lastName'
+                value={user.lastName}
+                onChange={(e)=> handleChange(e)}
+                />
+            </div>
+
+            <div className='reg-bottom'>
+                <input
+                placeholder='Company name (optional)'
+                type='text'
+                name='companyName'
+                value={user.companyName}
+                onChange={(e)=> handleChange(e)}
+                />
+
+                {errors.email ? (<span className='error-text'>{errors.email.message}</span>) : null}
+                <input
+                placeholder='Email Address'
+                type='email'
+                name='email'
+                value={user.email}
+                onChange={(e)=> handleChange(e)}
+                />
+
+                {errors.password ? (<span className='error-text'>{errors.password.message}</span>) : null}
+                <input
+                placeholder='Password'
+                type='password'
+                name='password'
+                value={user.password}
+                onChange={(e)=> handleChange(e)}
+                />
+
+                {errors.confirmPassword ? (<span className='error-text'>{errors.confirmPassword.message}</span>) : null}
+                <input
+                placeholder='Confirm Password'
+                type='password'
+                name='confirmPassword'
+                value={user.confirmPassword}
+                onChange={(e)=> handleChange(e)}
+                />
+            </div>
+
+            <button type='submit'>Sign Up</button>
+        </form>
+        <p>Already have an account? <Link to = {'/login'}>Login here.</Link></p>
         </div>
-
-        <div className='reg-bottom'>
-            <input
-            placeholder='Company name (optional)'
-            type='text'
-            name='companyName'
-            value={user.companyName}
-            onChange={(e)=> handleChange(e)}
-            />
-
-            {errors.email ? (<span className='error-text'>{errors.email.message}</span>) : null}
-            <input
-            placeholder='Email Address'
-            type='email'
-            name='email'
-            value={user.email}
-            onChange={(e)=> handleChange(e)}
-            />
-
-            {errors.password ? (<span className='error-text'>{errors.password.message}</span>) : null}
-            <input
-            placeholder='Password'
-            type='password'
-            name='password'
-            value={user.password}
-            onChange={(e)=> handleChange(e)}
-            />
-
-            {errors.confirmPassword ? (<span className='error-text'>{errors.confirmPassword.message}</span>) : null}
-            <input
-            placeholder='Confirm Password'
-            type='password'
-            name='confirmPassword'
-            value={user.confirmPassword}
-            onChange={(e)=> handleChange(e)}
-            />
-        </div>
-
-        <button type='submit'>Sign Up</button>
-      </form>
-      <p>Already have an account? <Link to = {'/login'}>Login here.</Link></p>
-    </div>
+    </dialog>
   )
 }
 
