e9811a2303410d21f65bf283dd49563d9c61737d
Michael MacLean <mrmaclean89@gmail.com>
2022-10-19 02:53:57 -0700

    Added locations to Map component. Styled Locations component.

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -6,8 +6,15 @@ const Locations = () => {
     
     
     <div>
-      <h2>Where to find Yola</h2>
-      <Map/>
+      <h2 className="font-bold text-5xl mx-auto text-center">Where to find Yola</h2>
+      <div className="flex flex-col items-center">
+        <p className="text-center mx-12 my-12 w-5/6">Find us in a store near you! As we grow, we are adding new locations to the map frequently. 
+          If you would like to know which stores we supply, or you would like us to supply a store near you, 
+          go to our "Contact Us" page and tell us where you are from and your preferred retail stores. 
+          We appreciate your input!
+        </p>
+        <Map className="w-2/3 mx-auto"/>
+      </div>
     </div>
   )
 }
