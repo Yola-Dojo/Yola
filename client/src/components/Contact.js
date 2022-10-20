6b9334b4155d4d50b5f67aed4999748caebac7da
Michael MacLean <mrmaclean89@gmail.com>
2022-10-20 15:30:43 -0700

    Cleared inputs on submit of Contact form.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -101,7 +101,10 @@ const logout = (e)=>{
       }
     )
   e.target.reset()
-
+  setCustName("")
+  setCustConcerns("")
+  setCustEmail("")
+  setCustLocation("")
   }
 
 
@@ -154,7 +157,7 @@ const logout = (e)=>{
               ></textarea>
 
               <div>
-                <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 
+                <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 
                 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600
                  dark:hover:bg-green-700 dark:focus:ring-green-800 my-5">Submit</button>
               </div>
