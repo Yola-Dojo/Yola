a8f6666efafa9e2e6c034ed001a58604680990eb
Michael MacLean <mrmaclean89@gmail.com>
2022-10-20 16:01:37 -0700

    Enlarged text on Checkout page. Fixed styling on About page.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,8 +10,8 @@ const About = () => {
     <div>
       <section class="overflow-hidden text-gray-700">
         <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
-          <div class="flex flex-wrap -m-1 md:-m-2">
-            <p className="w-6/12 text-center text-4xl pr-10 pt-6 font-light tracking-wide flex-wrap">
+          <div class="flex flex-col items-center">
+            <p className="w-11/12 text-center text-3xl mb-10 pt-6 font-light tracking-wide flex-wrap">
               S&B Sustainable Products LLC, headquartered in Atherton, California, was founded in 2014 by its two founders Steve Hooper and Bob MacLean.<br/>
               With the incredible growth in sales of yogurt products throughout the United States in the last 15 years, it was clear that consumers were looking for healthier, good tasting, multi-use dairy products. But there was one food category that had lagged behind: Dairy Toppings. 
               S&B set out to upgrade that category with a healthy, “clean ingredient”, competitive product that was unique, felt better and tasted better.
