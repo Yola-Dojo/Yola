0dd829ea0ef8e1bace5e5c448bc569df1ad84d73
Michael MacLean <mrmaclean89@gmail.com>
2022-10-20 04:41:09 -0700

    Made some CSS changes to LandingPage, Contact, AddProduct.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -110,10 +110,11 @@ const logout = (e)=>{
 
 
   return (
-    <div className ='contact-container'>
+    <div>
+      <h1 className="text-5xl font-bold mb-10 text-center">Contact Us</h1>
+      <div className ='contact-container'>
         <div className='left-col'>
-            <h1>Contact us</h1>
-            <p style={{wordWrap:'break-word'}}>Thank you for your interest in Yola! We value your input. Your engagement and feedback are invaluable to our growth. By telling us where you are from, we get an idea of where to supply Yola in the future. To all questions and comments, we're committed to responding promptly. Finally, please let us know how you heard of us!</p>
+          <p style={{wordWrap:'break-word'}}>Thank you for your interest in Yola! We value your input. Your engagement and feedback are invaluable to our growth. By telling us where you are from, we get an idea of where to supply Yola in the future. To all questions and comments, we're committed to responding promptly. Finally, please let us know how you heard of us!</p>
         </div>
         <form ref={form} className='contact-form' onSubmit={submitHandle}>
         {thank ? <h4 style={{color: 'green'}}>{thank}</h4> : null}
@@ -143,6 +144,7 @@ const logout = (e)=>{
               />
 
               <textarea 
+              className="rounded-lg border border-gray-300"
               rows='10' cols='50'
               style={{marginTop:'20px'}}
               name = 'message'
@@ -152,10 +154,13 @@ const logout = (e)=>{
               ></textarea>
 
               <div>
-              <button className='contact-btn'>Submit</button>
+                <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 
+                focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600
+                 dark:hover:bg-green-700 dark:focus:ring-green-800 my-5">Submit</button>
               </div>
-          </div>
-        </form>
+            </div>
+          </form>
+      </div>
     </div>
   )
 }
